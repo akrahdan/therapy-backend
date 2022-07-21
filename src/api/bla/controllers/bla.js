16adc00a9da5cc395446026db3dbee8b93425c75
Samuel Akrah <akrah001@d.umn.edu>
2022-07-21 12:06:04 -0500

    Dump stuff

---
+++ src/api/bla/controllers/bla.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  bla controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::bla.bla');
---
+++ src/api/bla/routes/bla.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bla router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bla.bla');
